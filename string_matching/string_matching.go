1ba5c4cdb9901cc8a3996996e37491c3a9f1cbb4
User <thomasvcole@gmail.com>
2024-02-20 21:03:16 -0700

    struggling to remember how to get going in the golang.

--- string_matching/string_matching.go
+++ string_matching/string_matching.go
@@ -1,1 +1,31 @@
-package string_matching
+package main
+
+import (
+	"fmt"
+	"io"
+	"math/big"
+)
+
+func where_hash_present(in_str string, hash_len int, search_hash big.Int) {
+	// finish this later.
+}
+
+func main() {
+	var isPattern bool
+	for {
+		var input string
+		_, err := fmt.Scan(&input)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			// Do nothing, not my problem you did something bad.
+		}
+		if isPattern {
+			// Finish later.
+		} else {
+			// Finish later.
+		}
+
+	}
+}
