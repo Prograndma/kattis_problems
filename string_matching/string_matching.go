854e1272425ea8a50fc3dd128cdbaa3d009df239
User <thomasvcole@gmail.com>
2024-02-26 21:27:57 -0700

    Adding big.ints was... so very very annoying. Once I figured it out, things made sense though. Maybe it was the big ints, or maybe it was something else but this seems to have fixed the outputs in at least some sitatuions, but not all of them. Whatever.

--- string_matching/string_matching.go
+++ string_matching/string_matching.go
@@ -3,7 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"math"
+	"math/big"
 	"os"
 	"unicode"
 )
@@ -11,105 +11,97 @@ import (
 var BASE = 29.
 var MOD = 1_000_000_007.
 
-func charsToArbitraryNumbers(inStr string) []int {
-	var returnArray []int
-	// So I have an idea, don't use this awful slice. You know the length of the array, you have
-	// the indices. Use them. Slices are awful. append uses slices! So like yeah. Update this and
-	// see what type of performance boost you can get when you're done.
+func charsToArbitraryNumbers(inStr string) []*big.Int {
+	var returnArray []*big.Int
 	for _, char := range inStr {
-		if unicode.IsUpper(char) {
-			returnArray = append(returnArray, int(char)-66+28)
+		if unicode.IsLetter(char) {
+			if unicode.IsUpper(char) {
+				returnArray = append(returnArray, big.NewInt(int64(char)-66+28))
+			} else {
+				returnArray = append(returnArray, big.NewInt(int64(char)-96))
+			}
 		} else {
-			returnArray = append(returnArray, int(char)-96)
+			returnArray = append(returnArray, big.NewInt(int64(char)+53))
 		}
 	}
 	return returnArray
 }
 
-func hashy(inStr string) int {
+func hashy(inStr string) *big.Int {
 	s := charsToArbitraryNumbers(inStr)
-	var curHash = 0
-	for i := range len(inStr) {
-		curHash = (curHash*int(BASE) + s[i]) % int(MOD)
+	var curHash = big.NewInt(0)
+	for i := 0; i < len(inStr); i++ {
+		//curHash = (curHash*int(BASE) + s[i]) % big.Int(MOD)
+		curHash = curHash.Mul(curHash, big.NewInt(int64(BASE)))
+		curHash = curHash.Add(curHash, s[i])
+		curHash = curHash.Mod(curHash, big.NewInt(int64(MOD)))
 	}
 	return curHash
 }
 
-func whereHashPresent(inStr string, hashLen int, searchHash int) []int {
-	maxPow := math.Pow(BASE, float64(hashLen))
-	maxPow = math.Mod(maxPow, MOD)
+func whereHashPresent(inStr string, hashLen int, searchHash *big.Int) []*big.Int {
+	var base = big.NewInt(29)
+	var mod = big.NewInt(1_000_000_007)
+	maxPow := base.Exp(base, big.NewInt(int64(hashLen-1)), mod)
 	var s = charsToArbitraryNumbers(inStr)
-	var where []int
-	var curHash int
-	//fmt.Println(inStr)
-	//fmt.Println(hashLen)
-	//fmt.Println(s)
-	for i := range hashLen {
-		curHash = (curHash*int(BASE) + s[i]) % int(MOD)
+	var where []*big.Int
+	var curHash = big.NewInt(0)
+	for i := 0; i < hashLen; i++ {
+		//curHash = (curHash*int(BASE) + s[i]) % big.Int(MOD)
+		curHash = curHash.Mul(curHash, big.NewInt(int64(BASE)))
+		curHash = curHash.Add(curHash, s[i])
+		curHash = curHash.Mod(curHash, big.NewInt(int64(MOD)))
 	}
 	// since we don't know how big where will be this might be better, we do have an upper limit though.
 	// Which is faster? allocating bigger memory? Or dealing with slices?
-	//fmt.Println(curHash)
-	//fmt.Println(searchHash)
-	if curHash == searchHash {
-		where = append(where, 0)
+	if curHash.Cmp(searchHash) == 0 {
+		where = append(where, big.NewInt(0))
 	}
 
 	for i := hashLen; i < len(s); i++ {
-		//curHash = ((curHash-s[i-hashLen]*int(maxPow))*int(BASE) + s[i]) % int(MOD)
-		//fmt.Println(i, hashLen, maxPow)
-		//fmt.Println(curHash)
-		curHash -= s[i-hashLen]
-		//fmt.Println(curHash)
-		curHash *= int(maxPow)
-		//fmt.Println(curHash)
-		curHash *= int(BASE)
-		//fmt.Println(curHash)
-		curHash += s[i]
-		//fmt.Println(curHash)
-		curHash %= int(MOD)
-		//fmt.Println(curHash)
-		//fmt.Println(searchHash)
-		if curHash == searchHash {
-			where = append(where, i-hashLen+1)
+		//curHash = ((curHash-s[i-hashLen]*int(maxPow))*int(BASE) + s[i]) % big.Int(MOD)
+		var temp = s[i-hashLen].Mul(s[i-hashLen], maxPow)
+		curHash = curHash.Sub(curHash, temp)
+		curHash = curHash.Mul(curHash, big.NewInt(int64(BASE)))
+		curHash = curHash.Add(curHash, s[i])
+		curHash = curHash.Mod(curHash, big.NewInt(int64(MOD)))
+		//curHash -= s[i-hashLen] * big.Int(maxPow)
+		//curHash *= big.Int(BASE)
+		//curHash += s[i]
+		//curHash %= big.Int(MOD)
+		//if curHash == searchHash {
+		if curHash.Cmp(searchHash) == 0 {
+			where = append(where, big.NewInt(int64(i-hashLen+1)))
 		}
 	}
-	//fmt.Println(where)
 	return where
 }
 
 func main() {
 	var isPattern = true
 	var lineLen = 0
-	var lineHash = 0
-	//var doneThis = 1
+	var lineHash *big.Int
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
-		//var input string
-		//_, err := fmt.Scanln(&input)
 		scanner.Scan()
 		// Holds the string that scanned
 		input := scanner.Text()
-		//if len(text) != 0 {
 		if len(input) == 0 {
 			break
 		}
 		if isPattern {
+			//fmt.Println(input)
 			isPattern = false
 			lineHash = hashy(input)
 			lineLen = len(input)
 		} else {
+			isPattern = true
 			locations := whereHashPresent(input, lineLen, lineHash)
-			//fmt.Println(locations)
 			for location := range locations {
 				fmt.Print(locations[location], " ")
 			}
 			fmt.Println("")
-			isPattern = true
 		}
-
-		//fmt.Println("DONE THIS: ", doneThis, " TIMES")
-		//doneThis += 1
 
 	}
 }
