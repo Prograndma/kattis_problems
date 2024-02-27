50e13d7001dd406a1c089d6209cd23ae35170cef
User <thomasvcole@gmail.com>
2024-02-26 21:26:19 -0700

    Changing thigns to match the go code better, to make it easier to compare them and try to find why one works and one doesn't.

--- string_matching/string_matching.py
+++ string_matching/string_matching.py
@@ -1,15 +1,42 @@
 import sys
-from sys import stdin
 
 MOD = 1_000_000_007
 BASE = 29
 
 
+def chars_to_arbitrary_numbers(in_str):
+    return_array = []
+    for char in in_str:
+        if char.isalpha():
+            if char.isupper():
+                return_array.append(ord(char) - 66 + 28)
+            else:
+                return_array.append(ord(char) - 96)
+        else:
+            return_array.append(53)
+
+    return return_array
+
+
+def hashy(in_str):
+    # s = list(map(lambda x: ord(x) - 96 if x.islower() else ord(x) - 66 + 26, in_str))
+    # s = [ord(x) - 96 if x.islower() else ord(x) - 66 + 28 for x in in_str]
+    s = chars_to_arbitrary_numbers(in_str)
+
+    cur_hash = 0
+    for i in range(len(in_str)):
+        cur_hash = (cur_hash * BASE + s[i]) % MOD
+    return cur_hash
+
+
 def where_hash_present(in_str, hash_len, search_hash):
-    max_pow = pow(BASE, hash_len - 1, MOD)
+    max_pow = pow(BASE, hash_len - 1)
+    max_pow = max_pow % MOD
+    # print(max_pow)
     # s = [int(ord(c) - 96) for c in in_str]
     # s = list(lambda x: ord(x) - 96 if x.islower() else ord(x) - 66 + 26, in_str)
-    s = [ord(x) - 96 if x.islower() else ord(x) - 66 + 26 for x in in_str]
+    # s = [ord(x) - 96 if x.islower() else ord(x) - 66 + 28 for x in in_str]
+    s = chars_to_arbitrary_numbers(in_str)
     # Compute the initial hash value for the first substring of length length_of_hash
     where = []
     cur_hash = 0
@@ -24,16 +51,6 @@ def where_hash_present(in_str, hash_len, search_hash):
         if cur_hash == search_hash:
             where.append(i - hash_len + 1)
     return where
-
-
-def hashy(in_str):
-    ### s = list(map(lambda x: ord(x) - 96 if x.islower() else ord(x) - 66 + 26, in_str))
-    s = [ord(x) - 96 if x.islower() else ord(x) - 66 + 26 for x in in_str]
-
-    cur_hash = 0
-    for i in range(len(in_str)):
-        cur_hash = (cur_hash * BASE + s[i]) % MOD
-    return cur_hash
 
 
 is_pattern = True
