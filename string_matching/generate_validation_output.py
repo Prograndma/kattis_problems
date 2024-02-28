b7b43175f1757a22b7e54a1f7e0145b27dee9f79
User <thomasvcole@gmail.com>
2024-02-27 20:50:50 -0700

    fixing the error where a trailing newline would be added to the numbers.

--- string_matching/generate_validation_output.py
+++ string_matching/generate_validation_output.py
@@ -5,15 +5,20 @@ END = ".txt"
 TEST_FILE = "test_strings.txt"
 
 
-def generate_ouput_for_chars_to_arbitrary_numbers():
+def list_to_space_separated_strings(to_convert):
+    return " ".join(list(map(str, to_convert))) + "\n"
+
+
+def generate_output_for_chars_to_arbitrary_numbers():
     with open(TEST_FILE, "r") as file:
         with open(f"{PRE}chars_to_arbitrary_numbers{END}", "w") as write_file:
             for line in file:
-                write_file.write(chars_to_arbitrary_numbers(line))
+                numbers = chars_to_arbitrary_numbers(line)
+                write_file.write(list_to_space_separated_strings(numbers))
 
 
 def main():
-    generate_ouput_for_chars_to_arbitrary_numbers()
+    generate_output_for_chars_to_arbitrary_numbers()
 
 
 if __name__ == "__main__":
