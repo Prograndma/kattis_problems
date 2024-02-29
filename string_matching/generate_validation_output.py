0ee0cc908353b8fb4b12d33dc44b0bef2476880c
User <thomasvcole@gmail.com>
2024-02-29 15:13:41 -0700

    generating output for where function

--- string_matching/generate_validation_output.py
+++ string_matching/generate_validation_output.py
@@ -1,8 +1,9 @@
-from string_matching import chars_to_arbitrary_numbers, hashy
+from string_matching import chars_to_arbitrary_numbers, hashy, where_hash_present
 
 PRE = "python_output_for_"
 END = ".txt"
 TEST_FILE = "test_strings.txt"
+TEST_SEARCH_FILE = "test_search_strings.txt"
 
 
 def list_to_space_separated_strings(to_convert):
@@ -25,9 +26,22 @@ def generate_output_for_hashy():
                 write_file.write(str(hashed) + "\n")
 
 
+def generate_output_for_where_hash_present():
+    with open(TEST_SEARCH_FILE) as file:
+        with open(f"{PRE}where_hash_present{END}", "w") as write_file:
+            lines = [line for line in file]
+            for i in range(0, len(lines), 2):           # every other element.
+                search = lines[i].strip()
+                line = lines[i + 1].strip()             # assume file has an even number of lines.
+                search_hash = hashy(search)
+                where = where_hash_present(line, len(search), search_hash)
+                write_file.write(list_to_space_separated_strings(where))
+
+
 def main():
     generate_output_for_chars_to_arbitrary_numbers()
     generate_output_for_hashy()
+    generate_output_for_where_hash_present()
 
 
 if __name__ == "__main__":
