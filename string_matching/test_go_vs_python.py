ce6315b4ab35a0d19d84e570b8537da917b512d9
User <thomasvcole@gmail.com>
2024-02-28 21:09:15 -0700

    Way to test hashy. Hashy is consistent between the two.

--- string_matching/test_go_vs_python.py
+++ string_matching/test_go_vs_python.py
@@ -1,5 +1,5 @@
 def test_chars_to_arbitrary_numbers():
-    print("Testing chars_to_arbitrary_numbers\n")
+    print("Testing chars_to_arbitrary_numbers")
     with open("test_strings.txt") as file:
         inputs = [line for line in file]
 
@@ -15,12 +15,33 @@ def test_chars_to_arbitrary_numbers():
             print(f"py_out (len={len(py_out.split())}): {py_out}")
             print(f"go_out (len={len(go_out.split())}): {go_out}\n")
             mis_matches += 1
-    print(f"{mis_matches=}")
-    print(f"total_tests={len(inputs)}")
+    print(f"{mis_matches}/{len(inputs)} tests failed\n")
+
+
+def test_hashy():
+    print("Testing hashy")
+    with open("test_strings.txt") as file:
+        inputs = [line for line in file]
+
+    with open("python_output_for_hashy.txt") as file:
+        python_outputs = [line for line in file]
+
+    with open("goOutputForHashy.txt") as file:
+        go_outputs = [line for line in file]
+    mis_matches = 0
+    for in_val, py_out, go_out in zip(inputs, python_outputs, go_outputs):
+        if py_out != go_out:
+            print(f"An error for input (len={len(in_val)}): {in_val}")
+            print(f"py_out (len={len(py_out.split())}): {py_out}")
+            print(f"go_out (len={len(go_out.split())}): {go_out}\n")
+            mis_matches += 1
+    print(f"{mis_matches}/{len(inputs)} tests failed\n")
+
 
 
 def main():
     test_chars_to_arbitrary_numbers()
+    test_hashy()
     print("ALL DONE")
 
 
