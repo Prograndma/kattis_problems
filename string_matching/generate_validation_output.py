7c9583f3770277b9d7fb4435fecbde51972eda3c
User <thomasvcole@gmail.com>
2024-02-28 20:59:59 -0700

    Added a way to create the out file for the hashy function

--- string_matching/generate_validation_output.py
+++ string_matching/generate_validation_output.py
@@ -1,4 +1,4 @@
-from string_matching import chars_to_arbitrary_numbers
+from string_matching import chars_to_arbitrary_numbers, hashy
 
 PRE = "python_output_for_"
 END = ".txt"
@@ -17,8 +17,17 @@ def generate_output_for_chars_to_arbitrary_numbers():
                 write_file.write(list_to_space_separated_strings(numbers))
 
 
+def generate_output_for_hashy():
+    with open(TEST_FILE) as file:
+        with open(f"{PRE}hashy{END}", "w") as write_file:
+            for line in file:
+                hashed = hashy(line)
+                write_file.write(str(hashed) + "\n")
+
+
 def main():
     generate_output_for_chars_to_arbitrary_numbers()
+    generate_output_for_hashy()
 
 
 if __name__ == "__main__":
