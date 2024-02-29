7f582547157276dbeb57a373b82220d74ea2d127
User <thomasvcole@gmail.com>
2024-02-28 20:38:08 -0700

    Finished the chars_to_arbitrary_numbers compare function.

--- string_matching/test_go_vs_python.py
+++ string_matching/test_go_vs_python.py
@@ -4,14 +4,22 @@ def test_chars_to_arbitrary_numbers():
         inputs = [line for line in file]
 
     with open("python_output_for_chars_to_arbitrary_numbers.txt") as file:
-        python_outputs = [line for line in file]
+        python_outputs = []
+        for line in file:
+            line = line.strip()
+            index = line.rfind(" ")
+            line_with_last_number_removed = line[:index].strip()
+            python_outputs.append(line_with_last_number_removed)
+        # python_outputs = [line[:-] for line in file]
+    with open("python_output_for_chars_to_arbitrary_numbers.txt") as file:
+        python_farts = [line for line in file]
 
     with open("goOutputForcharsToArbitraryNumbers.txt") as file:
-        go_outputs = [line for line in file]
+        go_outputs = [line[:-1] for line in file]
     mis_matches = 0
-    for input, py_out, go_out in zip(inputs, python_outputs, go_outputs):
+    for in_val, py_out, go_out in zip(inputs, python_outputs, go_outputs):
         if py_out != go_out:
-            print(f"An error for input (len={len(input)}): {input}")
+            print(f"An error for input (len={len(in_val)}): {in_val}")
             print(f"py_out (len={len(py_out.split())}): {py_out}")
             print(f"go_out (len={len(go_out.split())}): {go_out}\n")
             mis_matches += 1
