b44dd595f17b1e17d0a0aa6bf42733e9f7b89eaa
User <thomasvcole@gmail.com>
2024-03-04 19:22:04 -0700

    comparing files, 5 differences found but all have common output but uncommon differences.

--- string_matching/test_go_vs_python.py
+++ string_matching/test_go_vs_python.py
@@ -1,6 +1,10 @@
+TEST_FILE = "test_strings.txt"
+TEST_SEARCH_FILE = "test_search_strings.txt"
+
+
 def test_chars_to_arbitrary_numbers():
     print("Testing chars_to_arbitrary_numbers")
-    with open("test_strings.txt") as file:
+    with open(TEST_FILE) as file:
         inputs = [line for line in file]
 
     with open("python_output_for_chars_to_arbitrary_numbers.txt") as file:
@@ -20,7 +24,7 @@ def test_chars_to_arbitrary_numbers():
 
 def test_hashy():
     print("Testing hashy")
-    with open("test_strings.txt") as file:
+    with open(TEST_FILE) as file:
         inputs = [line for line in file]
 
     with open("python_output_for_hashy.txt") as file:
@@ -38,10 +42,39 @@ def test_hashy():
     print(f"{mis_matches}/{len(inputs)} tests failed\n")
 
 
+def test_where_hash_present():
+    print("Testing where_hash_present")
+    with open(TEST_SEARCH_FILE) as file:
+        i = 0
+        inputs = []
+        to_append = []
+        for line in file:
+            if i % 2 == 0:
+                to_append = [line]
+            else:
+                to_append.append(line)
+                inputs.append(to_append)
+            i += 1
+
+    with open("python_output_for_where_hash_present.txt") as file:
+        python_outputs = [line for line in file]
+
+    with open("goOutputForWhereHashPresent.txt") as file:
+        go_outputs = [line for line in file]
+    mis_matches = 0
+    for i, (in_val, py_out, go_out) in enumerate(zip(inputs, python_outputs, go_outputs)):
+        if py_out != go_out:
+            print(f"An error for {i*2 + 1} and {i*2 + 2}th input (len={len(in_val[1])}): {in_val}")
+            print(f"py_out (len={len(py_out.split())}): {py_out}")
+            print(f"go_out (len={len(go_out.split())}): {go_out}\n")
+            mis_matches += 1
+    print(f"{mis_matches}/{len(inputs)} tests failed\n")
+
 
 def main():
     test_chars_to_arbitrary_numbers()
     test_hashy()
+    test_where_hash_present()
     print("ALL DONE")
 
 
