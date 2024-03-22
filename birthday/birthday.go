8ae135ca0eb310f8487e71e2dfa147548833d1d7
User <thomasvcole@gmail.com>
2024-03-21 21:42:51 -0600

    This... worked. But? It wasn't faster than the python at all, it also seem to me that it might be bugged. Without the part that counts the connections, it fails. The python code can work without it. I'm too tired to look into it too much, dealing with all the pointer nonsense was too hard. Surely I should have just used a struct or something.

--- birthday/birthday.go
+++ birthday/birthday.go
@@ -2,8 +2,61 @@ package main
 
 import "fmt"
 
+func min(one, two int) int {
+	if one < two {
+		return one
+	}
+	return two
+}
+
+func _bridgeHelper(graph [][]int, node int, time *int, visited *[]bool, parent, low, disc *[]int) bool {
+	(*visited)[node] = true
+	(*disc)[node] = *time
+	(*low)[node] = *time
+	*time += 1
+
+	for _, connectedNode := range graph[node] {
+		if !(*visited)[connectedNode] {
+			(*parent)[connectedNode] = node
+			_bridgeHelper(graph, connectedNode, time, visited, parent, low, disc)
+			(*low)[node] = min((*low)[node], (*low)[connectedNode])
+
+			if (*low)[connectedNode] > (*disc)[node] {
+				return true
+			}
+		} else if connectedNode != (*parent)[node] {
+			(*low)[node] = min((*low)[node], (*disc)[connectedNode])
+		}
+	}
+
+	return false
+}
+
+func graphHasABridge(graph [][]int, numNodes int) bool {
+	time := 0
+	parent := make([]int, numNodes)
+	disc := make([]int, numNodes)
+	low := make([]int, numNodes)
+	visited := make([]bool, numNodes)
+	for i := range parent {
+		parent[i] = -1
+		disc[i] = numNodes + 1
+		low[i] = numNodes + 1
+		visited[i] = false
+	}
+
+	for i := 0; i < numNodes; i++ {
+		if !visited[i] {
+			if _bridgeHelper(graph, i, &time, &visited, &parent, &low, &disc) {
+				return true
+			}
+		}
+	}
+	return false
+}
+
 func analyzeGraph(numNodes, numEdges int) string {
-	nodes := make([]int, numNodes)
+	graph := make([][]int, numNodes)
 	var from = -1
 	var to = -1
 	for i := 0; i < numEdges; i++ {
@@ -12,14 +65,17 @@ func analyzeGraph(numNodes, numEdges int) string {
 			fmt.Println("Error occurred:", err)
 			return "err"
 		}
-		nodes[from] += 1
-		nodes[to] += 1
+		graph[from] = append(graph[from], to)
+		graph[to] = append(graph[to], from)
 
 	}
-	for _, num := range nodes {
-		if num < 2 {
+	for _, connections := range graph {
+		if len(connections) < 2 {
 			return "yes"
 		}
+	}
+	if graphHasABridge(graph, numNodes) {
+		return "yes"
 	}
 	return "no"
 }
